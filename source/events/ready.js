ff4b1de42766ec74c7e91e905164060199112968
Akash <oofakash@duck.com>
2023-07-27 17:24:00 +0530

    added activity

--- source/events/ready.js
+++ source/events/ready.js
@@ -1,9 +1,16 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
+
+		let status = {
+			name: 'with Puffy',
+			type: ActivityType.Playing
+		}
+
+		client.user.setActivity(status);
 		console.log('BOT IS ONLINE');
 	},
 };
